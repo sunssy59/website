ab3299efd27ff241f5180f1b54451634b2945382
Mamadou59 <msanoussy98@gmail.com>
2020-09-27 21:16:53 +0200

    Updates

---
+++ precache-manifest.b2966fb1bca5622ca8971ce59f251d7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97feb0c388c9d6d7853678ce378bb66",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "727c34fa1df238376168",
+    "url": "/website/static/css/main.864d0a0b.chunk.css"
+  },
+  {
+    "revision": "44f8ff74480a09e81940",
+    "url": "/website/static/js/2.93f18a3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/2.93f18a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727c34fa1df238376168",
+    "url": "/website/static/js/main.ca5136d0.chunk.js"
+  },
+  {
+    "revision": "a76f9486e46a4b11c390",
+    "url": "/website/static/js/runtime-main.42022ecf.js"
+  },
+  {
+    "revision": "e58f92868974899bdafba379f963339a",
+    "url": "/website/static/media/mamadou.e58f9286.jpg"
+  },
+  {
+    "revision": "bd0d7f87bb1d31b9490598c7b8b1412c",
+    "url": "/website/static/media/map.bd0d7f87.jpg"
+  },
+  {
+    "revision": "ac9959458f2154578f55407715fa33b2",
+    "url": "/website/static/media/shape.ac995945.svg"
+  }
+]);
